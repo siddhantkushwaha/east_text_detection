e2b438dc0b9efc7f59be113b57e8605d0b322a8e
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-11-01 22:48:39 +0530

    added callbacks for checkpointing and tb

--- train_v2.py
+++ train_v2.py
@@ -1,6 +1,9 @@
+import os
 import argparse
+from datetime import datetime
 
 import keras.backend as K
+from keras.callbacks import TensorBoard, ModelCheckpoint
 
 from adamw import AdamW
 from losses import dice_loss, rbox_loss
@@ -36,6 +39,12 @@ parser.add_argument('--gpu_list', type=str, default='0')
 FLAGS = parser.parse_args()
 
 
+def tensorboard_callback():
+    log_dir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+    return callback
+
+
 def main():
     train_data_generator = DataGenerator(input_size=FLAGS.input_size, batch_size=FLAGS.batch_size,
                                          data_path=FLAGS.training_data_path, FLAGS=FLAGS, is_train=True)
@@ -57,14 +66,26 @@ def main():
                       small_text_weight, east.target_score_map)
         ],
         loss_weights=[1., 1.],
-        optimizer=opt
+        optimizer=opt,
+        metrics=['accuracy']
     )
 
-    hist = east.model.fit_generator(
+    tb_callback = tensorboard_callback()
+    checkpoint_callback = ModelCheckpoint(
+        filepath=os.path.join(FLAGS.checkpoint_path, 'model-{epoch:02d}-{val_accuracy:.2f}.h5'),
+        save_best_only=True,
+        verbose=0,
+        save_weights_only=False,
+        period=FLAGS.save_checkpoint_epochs
+    )
+
+    east.model.fit_generator(
         generator=train_data_generator,
         epochs=FLAGS.max_epochs,
         steps_per_epoch=train_samples_count // FLAGS.batch_size,
         validation_data=validation_data_generator,
+
+        callbacks=[checkpoint_callback, tb_callback],
 
         workers=FLAGS.nb_workers,
         use_multiprocessing=True,
@@ -72,8 +93,6 @@ def main():
 
         verbose=1,
     )
-
-    print(hist)
 
 
 if __name__ == '__main__':
