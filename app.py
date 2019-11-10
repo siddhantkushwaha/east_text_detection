a0b997474bb80ce1a4b354dea75c35edc387ce74
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-11-10 11:26:53 +0530

    fixes to app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import logging
-
-from flask import Flask, request
-import numpy as np
+from flask import Flask, request, jsonify
 from PIL import Image
+
+import numpy as np
+import tensorflow as tf
+
+import logging
 
 from predict import load_model, process_image
 
@@ -17,19 +19,26 @@ def index():
 @app.route('/process', methods=['POST'])
 def process():
     image_buf = request.files['image']
-    image = Image.open(image_buf)
+    image = Image.open(image_buf).convert('RGB')
 
     # convert to image to numpy array
     image = np.array(image)
-    print(image.shape)
+    image = image[:, :, ::-1].copy()
 
-    boxes = process_image(model, image)
-    print(boxes)
+    global graph
+    with graph.as_default():
+        boxes = process_image(model, image)
 
-    return 'Ok.'
+    lines = []
+    for box in boxes:
+        line = box.reshape((8,)).tolist()
+        lines.append(line)
+
+    return jsonify(lines)
 
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.ERROR)
     model = load_model(model_path='models/east/model-funsd400.h5')
+    graph = tf.get_default_graph()
     app.run(host='0.0.0.0', port=6006)
