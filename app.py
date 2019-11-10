17408581dd4036b57665b6b7f7e5c5c9ae15465a
Siddhant Kushwaha <k16.siddhant@gmail.com>
2019-11-10 12:47:22 +0530

    small fixes

--- app.py
+++ app.py
@@ -20,10 +20,7 @@ def index():
 def process():
     image_buf = request.files['image']
     image = Image.open(image_buf).convert('RGB')
-
-    # convert to image to numpy array
     image = np.array(image)
-    image = image[:, :, ::-1].copy()
 
     global graph
     with graph.as_default():
@@ -39,6 +36,6 @@ def process():
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.ERROR)
-    model = load_model(model_path='models/east/model-funsd400.h5')
+    model = load_model(model_path='models/east/model-funsd150-icdar200.h5')
     graph = tf.get_default_graph()
     app.run(host='0.0.0.0', port=6006)
